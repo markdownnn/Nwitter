076cf6677e8422a2e3609914e329a7fd06f6f28f
Hyeok <mh960110@gmail.com>
2020-12-24 23:41:54 +0900

    Do initial firebase setting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_8LdUqMCctpMv276M7lYx4GvOkKZDPco",
+  authDomain: "nwitter-93e44.firebaseapp.com",
+  projectId: "nwitter-93e44",
+  storageBucket: "nwitter-93e44.appspot.com",
+  messagingSenderId: "44718088415",
+  appId: "1:44718088415:web:d4bc77a471210dec6d896a",
+};
+
+export default firebase.initializeApp(firebaseConfig);
