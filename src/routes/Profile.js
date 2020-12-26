d4629ba434df3461a1552d2011637722750baa0a
Hyeok <mh960110@gmail.com>
2020-12-26 22:01:22 +0900

    Add edit profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,12 +1,48 @@
-import React from "react";
-import { authService } from "../fbase";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { authService, dbService } from "../fbase";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const onLogOutClick = () => {
     authService.signOut();
   };
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .get();
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+  const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display name"
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update profile" />
+      </form>
       <button onClick={onLogOutClick}>Log out</button>
     </>
   );
